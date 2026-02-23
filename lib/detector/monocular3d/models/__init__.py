9fe9afff6e1e5b85c7edbbcf65b0aa7fa4bf93eb
Rohith Peddi <rxp190007@utdallas.edu>
2026-02-23 13:09:02 -0600

    [Monocular3D] Setup of 3D object detector - 46

--- lib/detector/monocular3d/models/__init__.py
+++ lib/detector/monocular3d/models/__init__.py
@@ -1,3 +1,3 @@
-from .dino_mono_3d import DinoV3Monocular3D, FactorizedMonocular3DHead, create_model
+from .dino_mono_3d import DinoV3Monocular3D, create_model
 
-__all__ = ["DinoV3Monocular3D", "FactorizedMonocular3DHead", "create_model"]
+__all__ = ["DinoV3Monocular3D", "create_model"]
