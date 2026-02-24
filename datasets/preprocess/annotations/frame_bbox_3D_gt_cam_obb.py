25a4101d3dc4021d0da0338b680d633c40e5ba71
Rohith Peddi <rxp190007@utdallas.edu>
2026-02-24 11:22:14 -0600

    63

--- datasets/preprocess/annotations/frame_bbox_3D_gt_cam_obb.py
+++ datasets/preprocess/annotations/frame_bbox_3D_gt_cam_obb.py
@@ -50,6 +50,10 @@ def rerun_frame_vis_camera_obb(
     """
     rr.init(app_id, spawn=True)
     
+    # Wait for the Rerun viewer to fully start before logging data
+    import time
+    time.sleep(3)
+    
     # Use RDF (Right-Down-Forward) for OpenCV camera convention
     rr.log("/", rr.ViewCoordinates.RDF)
     BASE = "camera_frame"
